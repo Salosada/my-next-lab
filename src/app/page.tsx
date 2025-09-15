d1cb5929bd37593801654cda179389104d118769
Losada <santiago.losadam@udea.edu.co>
2025-09-14 22:34:39 -0500

    Página de prueba antes de cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return <h1>¡Cambio automático con Vercel 🔄!</h1>;
 }
 */
-/*
+
   export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
@@ -21,7 +21,11 @@ export default function Home() {
       </footer>
     </main>
   );
-}*/
+}
+
+
+
+/*
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 text-white">
@@ -44,4 +48,4 @@ export default function Home() {
     </main>
   );
 }
-
+*/
